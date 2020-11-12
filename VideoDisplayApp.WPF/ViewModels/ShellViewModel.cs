7c431f7b950d492c380f0766c9a471455dcdbd23
Ákos Bujtás <bujtas95@gmail.com>
2020-11-12 14:07:13 +0100

    Add Newtondoft.Json and Caliburn.Micro references to the project and give a basic implementation of Bootstrapper

---
+++ VideoDisplayApp.WPF/ViewModels/ShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoDisplayApp.WPF.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private VideoDisplayViewModel _videoDisplayVM;
+
+        public ShellViewModel(VideoDisplayViewModel videoDisplayVM)
+        {
+            _videoDisplayVM = videoDisplayVM;
+            ActivateItem(_videoDisplayVM);
+        }
+    }
+}
--- VideoDisplayApp.WPF/Views/VideoDisplayView.xaml.cs
+++ VideoDisplayApp.WPF/Views/VideoDisplayView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VideoDisplayApp.WPF
+namespace VideoDisplayApp.WPF.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for VideoDisplayView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class VideoDisplayView : UserControl
     {
-        public MainWindow()
+        public VideoDisplayView()
         {
             InitializeComponent();
         }
